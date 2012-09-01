460f64f480448e19066383dd1dd9aa03ca4e7f61
Wojtek Zając <buterux@gmail.com>
2012-09-01 02:40:00 +0200

    Updates to styling! Darkness.

--- loop-single.php
+++ loop-single.php
@@ -33,26 +33,26 @@
 </style>
 
 		<div class="group">
-			<?php if($post->post_content != "") {?>
-			<div id="pattern-notes" class="mod">
-				<h3 class="label">Uwagi</h3>
-				<?php the_content(); ?>
-			</div>
-			<?php } ?>
-			
 			<div id="markup" class="mod">
-				<h3 class="label">HTML</h3> <a href="#" class="clip" title="select code for copying"><img src="<?php bloginfo('template_directory'); ?>/images/icon-copy.png" alt="copy" /></a>
+				<h3 class="label">HTML</h3>
 				<textarea class="mod-ta">
 <?php $key="html"; echo get_post_meta($post->ID, $key, true); ?>			
 				</textarea>
 			</div>
 			
 			<div id="style" class="mod">
-				<h3 class="label">CSS</h3> <a href="#" class="clip" title="select code for copying"><img src="<?php bloginfo('template_directory'); ?>/images/icon-copy.png" alt="copy" /></a>
+				<h3 class="label">CSS</h3>
 				<textarea id="css-code" class="mod-ta">
 <?php $key="css"; echo get_post_meta($post->ID, $key, true); ?>
 				</textarea>
 			</div>
+			
+			<?php if($post->post_content != "") {?>
+			<div id="pattern-notes" class="mod">
+				<h3 class="label">Uwagi</h3>
+				<?php the_content(); ?>
+			</div>
+			<?php } ?>
 		</div>
 
 <?php endwhile; // end of the loop. ?>
